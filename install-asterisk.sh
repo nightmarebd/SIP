1b610c42d73090d731c0138bc0754dbe280cca21
nightmarebd <nahid.com@gmail.com>
2023-11-21 01:59:49 +0600

    Update install-asterisk.sh
    asterisk 20 instead of 18

--- install-asterisk.sh
+++ install-asterisk.sh
@@ -3,7 +3,7 @@ sudo apt install wget build-essential subversion
 cd /usr/src/
 sudo wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-18-current.tar.gz
 sudo tar zxf asterisk-18-current.tar.gz
-cd asterisk-18.*/
+cd asterisk-20.*/
 sudo contrib/scripts/get_mp3_source.sh
 sudo contrib/scripts/install_prereq install
 sudo ./configure
